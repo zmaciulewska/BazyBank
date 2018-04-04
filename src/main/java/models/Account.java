bca35cf9b212f88b22494f5166b93fda66e0cec7
Zuzia <zmaciulewska@gmail.com>
2018-04-04 14:02:45 +0200

    models added

---
+++ src/main/java/models/Account.java
@@ -0,0 +1,58 @@
+package models;
+
+public class Account {
+
+    private Integer id;
+    private Integer idClient;
+    private String notes;
+    private long balance;
+
+    public Account(Integer id, Integer idClient, String notes, long balance) {
+        this.id = id;
+        this.idClient = idClient;
+        this.notes = notes;
+        this.balance = balance;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(Integer idClient) {
+        this.idClient = idClient;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", idClient=" + idClient +
+                ", notes='" + notes + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
---
+++ src/main/java/models/Client.java
@@ -0,0 +1,68 @@
+package models;
+
+public class Client {
+
+    private Integer id;
+    private String firstName, lastName, pesel, email;
+
+    public Client(Integer id, String firstName, String lastName, String pesel, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pesel = pesel;
+        this.email = email;
+
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
